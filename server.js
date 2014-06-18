dd126406bd6c55582c5b3635fa89b82c51779314
Danny Grove <danny@drgrovellc.com>
2014-06-18 01:26:02 -0400

    Fixes formatting

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 var express = require('express');
-    require('express-namespace');
+  require('express-namespace');
 var areas = require('./libs/areas'),
-    doorAccess = require('./libs/doorAccess'),
-    members = require('./libs/members'),
-    cards = require('./libs/cards')
-    config = require('./libs/config'),
-		response = require('./libs/response'),
-		employees = require('./libs/employees')
+  doorAccess = require('./libs/doorAccess'),
+  members = require('./libs/members'),
+  cards = require('./libs/cards')
+  config = require('./libs/config'),
+  response = require('./libs/response'),
+  employees = require('./libs/employees')
 
 
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-        res.send(200);
-    } else {
-        next();
-    }
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
 };
 
 var setResponseOptions = function(req, res, next) {
@@ -36,20 +36,19 @@ app.use(express.methodOverride());
 app.use(allowCrossDomain);
 app.use(setResponseOptions)
 app.namespace(config.app.namespace, function(){
-    app.get('/dooraccess', doorAccess.getAll);
-    app.get('/dooraccess/:id', doorAccess.getByMemberId);
-    app.get('/areas', areas.findAll);
-    app.get('/areas/:id', areas.getById);
-    app.get('/members', members.getAll);
-    app.get('/members/:memberid', members.getById);
-    app.post('/members/add/', members.add);
-    app.post('/members/update/', members.update);
-		app.del('/members/remove/:memberid', members.remove)
-    app.post('/members/toggleEnabled/:memberid', members.toggleEnabled);
-    app.post('/members/cards/add', cards.addCard);
-    app.del('/members/cards/remove/:id', cards.removeCard);
-		app.get('/employees', employees.getAll)
-
+  app.get('/dooraccess', doorAccess.getAll);
+  app.get('/dooraccess/:id', doorAccess.getByMemberId);
+  app.get('/areas', areas.findAll);
+  app.get('/areas/:id', areas.getById);
+  app.get('/members', members.getAll);
+  app.get('/members/:memberid', members.getById);
+  app.post('/members/add/', members.add);
+  app.post('/members/update/', members.update);
+  app.del('/members/remove/:memberid', members.remove)
+  app.post('/members/toggleEnabled/:memberid', members.toggleEnabled);
+  app.post('/members/cards/add', cards.addCard);
+  app.del('/members/cards/remove/:id', cards.removeCard);
+  app.get('/employees', employees.getAll)
 });
 app.listen(config.app.port);
 console.log('Listening on port ' + config.app.port);
